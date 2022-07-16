afe9bf68eb14c05007eaaa3cb520c4afc142f834
Katy Fernelius <kkin001@gmail.com>
2022-07-16 08:47:27 -0500

    Updated the create card function for remove capabilities

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 'use strict';
-//to refactor you need to get the entire form and do an onsubmit event
-//variables to get the elements on the page
+
 const destName = document.getElementById('dest-name');
 const locale = document.getElementById('location');
 const photo = document.getElementById('photo');
@@ -10,26 +9,14 @@ const myForm = document.getElementById('destination-form');
 const addBtn = document.getElementById('addBtn');
 const defaultImage =
   'https://s3.amazonaws.com/ae-lane-report/wp-content/uploads/2020/04/21121102/GettyImages-1160947136-1.jpg';
-//create functions for card creation
-//create function for edit
-//create function for remove
-
- myForm.addEventListener('submit', e => {
-  e.preventDefault();
+//create functions for card creation\
+function createCard() {
   const dNValue = destName.value;
   const locValue = locale.value;
   const photoVal = photo.value;
   const descValue = description.value;
 
   var newDiv = document.createElement('div');
-  newDiv.setAttribute('class', 'card-body');
-  var newBtn1 = document.createElement('button');
-  newBtn1.setAttribute('class', 'btn btn-primary');
-  newBtn1.innerText = 'Edit';
-  var newBtn2 = document.createElement('button');
-  newBtn2.setAttribute('class', 'btn btn-primary');
-  newBtn2.innerText = 'Remove';
-  
   newDiv.innerHTML = `
   <img src="${photoVal}" class='card-img-top' alt ="vacation photo"/>
   <div class='card-body'>
@@ -37,9 +24,30 @@ const defaultImage =
     <h5 class='card-title>${locValue}</h5>
     <p class='card-text'>${descValue}</p>
   `;
+  newDiv.setAttribute('class', 'card-body');
+
+  var newBtn1 = document.createElement('button');
+  newBtn1.setAttribute('class', 'btn btn-primary');
+  newBtn1.innerText = 'Edit';
+  newBtn1.addEventListener('click', () => {});
+
+  var newBtn2 = document.createElement('button');
+  newBtn2.setAttribute('class', 'btn btn-primary');
+  newBtn2.innerText = 'Remove';
+  newBtn2.addEventListener('click', () => {
+    newBtn2.parentElement.remove();
+  });
+
   newDiv.appendChild(newBtn1);
   newDiv.appendChild(newBtn2);
 
-  document.querySelector('form').reset();
   cardContainer.appendChild(newDiv);
+  document.querySelector('form').reset();
+}
+//create function for edit
+//create function for remove
+
+myForm.addEventListener('submit', e => {
+  e.preventDefault();
+  createCard();
 });
